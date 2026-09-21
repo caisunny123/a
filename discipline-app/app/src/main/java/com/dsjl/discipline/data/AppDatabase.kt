fe07e5aa6e551307433466831140d196fa019e11
caisunny123 <wzcaiqingshi@163.com>
2026-09-21 13:17:21 +0800

    Update AppDatabase.kt

--- discipline-app/app/src/main/java/com/dsjl/discipline/data/AppDatabase.kt
+++ discipline-app/app/src/main/java/com/dsjl/discipline/data/AppDatabase.kt
@@ -6,6 +6,7 @@ import androidx.room.Database
 import androidx.room.Delete
 import androidx.room.Entity
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.PrimaryKey
 import androidx.room.Query
 import androidx.room.Room
